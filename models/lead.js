73915cdc0b56fc819f48a5e2d43ad1aac2a57d58
Yallow <tylerbouma1@gmail.com>
2019-04-12 09:28:59 -0500

    Fixed syntax in lead model

--- models/lead.js
+++ models/lead.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true
         },
         email: {
-            type DataTypes.STRING,
+            type: DataTypes.STRING,
             allowNull: false
         },
     });
